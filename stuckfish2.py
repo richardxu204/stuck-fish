c34c4b24a7f2f931873508dbe5a635903d687712
Richardxcn5 <richardxcn5@gmail.com>
2021-04-25 22:15:06 -0700

    most chessboard functionality implemented, missing promotion

--- stuckfish2.py
+++ stuckfish2.py
@@ -1,6 +1,5 @@
 import sys, os
 import tkinter as tk
-#from tkinter import *
 import chess
 import math
 from PIL import ImageTk, Image
@@ -12,20 +11,26 @@ class ChessBoard(tk.Tk):
     def __init__(self, size=100):
         super().__init__()
         super().resizable(False, False)
-        self.fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w 0 1"
+        self.chess = chess.Board()
+        self.fen = self.chess.fen()
         self.path = os.getcwd()
         self.rows = 8
         self.columns = 8
         self.size = size
         self.color1 = "#DDB88C"
         self.color2 = "#A66D4F"
+        self.highlight_color1 = "#35C844"
+        self.highlight_color2 = "#78ED84"
         self.title('Stuckfish Engine 0.001')
         self.geometry('800x800')
+        
         #self.images = {}
         self.images = []
         self.piece_codes = {'b': "bblack", 'B': "bwhite", 'k': "kblack", 'K': "kwhite", 'n': "nblack", 'N': "nwhite", 'p': "pblack", 'P': "pwhite", 'q': "qblack", 'Q': "qwhite", 'r': "rblack", 'R': "rwhite"}
         self.position = [[0 for x in range(self.rows)] for y in range(self.columns)] 
         self.overlay = [[0 for x in range(self.rows)] for y in range(self.columns)] 
+        self.selection = 0
+        self.piece_square = ""
 
         self.canvas = tk.Canvas()
         self.canvas.pack(side="top", fill="both", expand=True, padx=0, pady=0)
@@ -35,27 +40,56 @@ class ChessBoard(tk.Tk):
         self.canvas.bind("<Button-1>", self.left_click)
 
 
-    def set_position(self, fen):
+    def set_fen(self, fen):
         self.fen = fen
 
     def get_position(self, position):
         return position[:(findnth(position, " ", 1) - 2)]
 
-    def get_piece(self, ):
-        print("test")
+    def get_piece(self, piece_x, piece_y):
+        return self.position[piece_x][piece_y]
+
+    def get_valid_moves(self):
+        moves = []
+        generator_moves = list(self.chess.legal_moves)
+        for move in generator_moves:
+            moves.append(move.uci())
+        
+        return moves
+        
+    def get_piece_moves(self, piece_pos):
+        piece_moves = []
+        for move in self.get_valid_moves():
+            if move.startswith(piece_pos):
+                piece_moves.append(move[-2:])
+        return piece_moves
 
     def left_click(self, event):
-        #print( "clicked at", (math.floor(event.x / 100)), (math.floor(event.y / 100)))
-        if self.overlay[(math.floor(event.y / 100))][(math.floor(event.x / 100))] == 1:
-            self.overlay[(math.floor(event.y / 100))][(math.floor(event.x / 100))] = 0
+        click_x = (math.floor(event.x / 100))
+        click_y = (math.floor(event.y / 100))
+        clicked_square = convert_col(click_x + 1) + str((8 - click_y))
+        if self.selection == 0:
+            self.overlay = [[0 for x in range(self.rows)] for y in range(self.columns)]
+            self.overlay[click_y][click_x] = 1
+            for move in self.get_piece_moves(clicked_square):
+                self.overlay[8 - int(move[1])][int(convert_col(move[:1])) - 1] = 2
+            self.draw_board()
+            self.draw_position()
+            self.selection = 1
+            self.piece_square = clicked_square
         else:
-            self.overlay[(math.floor(event.y / 100))][(math.floor(event.x / 100))] = 1
-        self.draw_board()
-        self.draw_position()
-        #self.canvas.create_oval(1, 1, 1, 1, fill="#476042", outline="#476042", width=10)
+            if clicked_square in self.get_piece_moves(self.piece_square):
+                self.chess.push(chess.Move.from_uci(self.piece_square + clicked_square))
+                self.fen = self.chess.fen()
+            self.piece_square = ""
+            self.overlay = [[0 for x in range(self.rows)] for y in range(self.columns)]
+            self.selection = 0
+            self.draw_board()
+            self.draw_position()
 
 
     def draw_board(self, perspective="w"):
+        self.canvas.delete("all")
         color = self.color2
         for row in range(self.rows):
             color = self.color1 if color == self.color2 else self.color2
@@ -65,7 +99,9 @@ class ChessBoard(tk.Tk):
                 x2 = x1 + self.size
                 y2 = y1 + self.size
                 if self.overlay[row][col] == 1:
-                    self.canvas.create_rectangle(x1, y1, x2, y2, outline="black", fill="green", tags="empty")
+                    self.canvas.create_rectangle(x1, y1, x2, y2, outline="black", fill=self.highlight_color1, tags="empty")
+                elif self.overlay[row][col] == 2:
+                    self.canvas.create_rectangle(x1, y1, x2, y2, outline="black", fill=self.highlight_color2, tags="empty")
                 else:
                     self.canvas.create_rectangle(x1, y1, x2, y2, outline="black", fill=color, tags="empty")
                 color = self.color1 if color == self.color2 else self.color2
@@ -73,11 +109,16 @@ class ChessBoard(tk.Tk):
         self.canvas.tag_raise("occupied")
         #self.canvas.pack(side="top", fill="both", expand=True, padx=0, pady=0)
 
+    def reset_position(self, perspective="w"):
+        self.position = [[0 for x in range(self.rows)] for y in range(self.columns)]
+         
+
     def draw_position(self, perspective="w"):
         #cycle through whole board
 
         #reset the stored position of the whole board for re-drawing of position
         self.position = [[0 for x in range(self.rows)] for y in range(self.columns)]
+        
         x_value = 0
         y_value = 0
         for piece in self.get_position(self.fen):
@@ -94,6 +135,8 @@ class ChessBoard(tk.Tk):
                 piece_id = self.canvas.create_image((x_value * 100) + 50, (y_value * 100) + 50, image=piece_img, tags="occupied", anchor="c")
                 self.images.append(piece_img)
             x_value = x_value + 1
+
+    
 
   
 
--- stuckfishsupport.py
+++ stuckfishsupport.py
@@ -3,3 +3,41 @@ def findnth(haystack, needle, n):
     if len(parts)<=n+1:
         return -1
     return len(haystack)-len(parts[-1])-len(needle)
+
+def convert_col(col):
+    if col == 1:
+        return "a"
+    elif col == 2:
+        return "b"
+    elif col == 3:
+        return "c"
+    elif col == 4:
+        return "d"
+    elif col == 5:
+        return "e"
+    elif col == 6:
+        return "f"
+    elif col == 7:
+        return "g"
+    elif col == 8:
+        return "h"
+    elif col == "a":
+        return 1
+    elif col == "b":
+        return 2
+    elif col == "c":
+        return 3
+    elif col == "d":
+        return 4
+    elif col == "e":
+        return 5
+    elif col == "f":
+        return 6
+    elif col == "g":
+        return 7
+    elif col == "h":
+        return 8
+    else:
+        #error
+        return
+
